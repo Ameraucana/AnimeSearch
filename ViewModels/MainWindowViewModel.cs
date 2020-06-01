ca6a62d900769d8f81c5419c98e07317f6363cf2
Wyandotte <daltdalt1218@comcast.net>
2020-05-31 21:37:28 -0400

    First commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace AnimeSearch.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            Content = new LoadingScreenViewModel();
+        }
+        private ViewModelBase content;
+        public ViewModelBase Content
+        {
+            get => content;
+            set => this.RaiseAndSetIfChanged(ref content, value);
+        }
+    }
+}
